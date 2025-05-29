9cb836b4f2d1cd263829d344876d62ae78012b50
Eldovyn <farras.pramudita@gmail.com>
2025-05-29 23:13:56 +0700

    style: update all endpoint

--- app/api/account_active.py
+++ app/api/account_active.py
@@ -4,7 +4,7 @@ from ..controllers import AccountActiveController
 account_active_router = Blueprint("account_active_router", __name__)
 
 
-@account_active_router.post("/short.me/auth/account-active/request")
+@account_active_router.post("/sludgify/auth/account-active/request")
 async def send_account_active_email():
     data = request.json
     timestamp = request.timestamp
@@ -12,7 +12,7 @@ async def send_account_active_email():
     return await AccountActiveController.send_account_active_email(email, timestamp)
 
 
-@account_active_router.get("/short.me/auth/account-active/status/<string:token>")
+@account_active_router.get("/sludgify/auth/account-active/status/<string:token>")
 async def user_account_active_information(token):
     timestamp = request.timestamp
     return await AccountActiveController.user_account_active_information(
@@ -20,7 +20,7 @@ async def user_account_active_information(token):
     )
 
 
-@account_active_router.get("/short.me/auth/account-active/verify/<string:token>")
+@account_active_router.get("/sludgify/auth/account-active/verify/<string:token>")
 async def get_user_account_active_verification(token):
     timestamp = request.timestamp
     return await AccountActiveController.get_user_account_active_verification(
@@ -28,7 +28,7 @@ async def get_user_account_active_verification(token):
     )
 
 
-@account_active_router.patch("/short.me/auth/account-active/active/<string:token>")
+@account_active_router.patch("/sludgify/auth/account-active/active/<string:token>")
 async def user_account_active_verification(token):
     timestamp = request.timestamp
     return await AccountActiveController.user_account_active_verification(
