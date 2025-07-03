18d69d3b3a130422f0c7815572b58af0f1fb7aef
Eldovyn <farras.pramudita@gmail.com>
2025-07-03 09:10:23 +0700

    refactor: remove file task

--- app/task.py
+++
@@ -1,8 +0,0 @@
-from . import celery_app
-from .utils import send_email
-
-
-@celery_app.task(name="send_email_task")
-def send_email_task(subject, recipients, body):
-    send_email(subject, recipients, body)
-    return f"send email {subject} to {recipients}"
